1c3ee9676228c33398dd9ece16679e70e0c2dffd
Andy Chang <>
2021-04-18 20:33:04 +0800

    project start

---
+++ src/test/java/com/abstractionizer/login/jwt/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abstractionizer.login.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
