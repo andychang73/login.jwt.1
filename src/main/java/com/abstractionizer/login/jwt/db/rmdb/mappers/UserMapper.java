86d7bb1f2186c8707a87126c194638d523eb5a31
Andy Chang <>
2021-04-18 20:55:16 +0800

    completed
    q
    <?xml version=1.0 encoding=UTF-8 ?>
    <mapper namespace=com.abstractionizer.studentinformationsystem.db.rmdb.mappers.UserCourseMapper>
    git commit -m completed

---
+++ src/main/java/com/abstractionizer/login/jwt/db/rmdb/mappers/UserMapper.java
@@ -0,0 +1,9 @@
+package com.abstractionizer.login.jwt.db.rmdb.mappers;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Mapper
+@Repository
+public class UserMapper {
+}
